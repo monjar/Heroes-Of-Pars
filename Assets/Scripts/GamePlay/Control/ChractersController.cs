884ae0cdd72cba37b260ebffb1907ea1bd727b82
amirali <amirali.mnj@gmail.com>
2021-03-17 22:27:40 +0330

    Added player control basics.

---
+++ Assets/Scripts/GamePlay/Control/ChractersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharactersContoller : MonoBehaviour
+{
+    protected NavMeshAgent navMeshAgent;
+    protected Camera characterCamera;
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        characterCamera = Camera.main;
+    }
+
+}
---
+++ Assets/Scripts/GamePlay/Control/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : CharactersContoller
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            MoveToSelectedPoint();
+        }
+    }
+
+    private void MoveToSelectedPoint()
+    {
+        var ray = characterCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
+            navMeshAgent.SetDestination(hit.point);
+    }
+}
